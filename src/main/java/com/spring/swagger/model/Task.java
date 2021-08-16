542fad12b66f8648dc1c0a7af69bec11e57fac4b
HaoWang95 <alanwang0028@gmail.com>
2021-08-16 14:15:23 +1000

    Update: createAt, lastModifiedAt config

--- src/main/java/com/spring/swagger/model/Task.java
+++ src/main/java/com/spring/swagger/model/Task.java
@@ -1,8 +1,11 @@
 package com.spring.swagger.model;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -23,10 +26,34 @@ public class Task {
     @Column(name = "completed")
     private Boolean completed;
 
+    @Column(name = "createdAt")
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @Column(name = "lastModifiedAt")
+    @LastModifiedDate
+    private LocalDate lastModifiedAt;
+
 
     public Task(String title, String content){
         this.title = title;
         this.content = content;
         this.completed = false;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        LocalDate now = LocalDate.now();
+        if(createdAt == null){
+            createdAt = now;
+        }
+        if(lastModifiedAt == null){
+            lastModifiedAt = LocalDate.now();
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        lastModifiedAt = LocalDate.now();
     }
 }
